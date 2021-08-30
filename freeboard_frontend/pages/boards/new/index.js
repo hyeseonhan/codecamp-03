90d8e3c80fb2f0f50fc44a9d8f19641b407642b9
hyeseon <hh@hyeseonui-MacBookAir.local>
2021-08-30 14:58:17 +0900

    New Folder

---
+++ freeboard_frontend/pages/boards/new/index.js
@@ -0,0 +1,9 @@
+export default function BoardsNewPage(){
+
+    return (
+        
+        <Title>게시물 등록</Title>
+        작성자: <IdInPut type ="text"/>
+        )
+
+}
---
+++ freeboard_frontend/styles/boards.js
@@ -0,0 +1,7 @@
+import styled from '@boards/styled'
+
+export const Title = styled.h1`
+    color: blue
+`
+
+export const IdInPut = styled.input``
