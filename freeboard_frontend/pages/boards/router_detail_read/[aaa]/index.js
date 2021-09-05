56bb2239a2836fee1b7bd2b7f4ea6f39041ca25c
hyeseon <h1225hs@gmail.com>
2021-09-06 01:46:54 +0900

    freeboard_frontend router_detail_read page css done

--- freeboard_frontend/pages/boards/router_detail_read/[aaa]/index.js
+++ freeboard_frontend/pages/boards/router_detail_read/[aaa]/index.js
@@ -1,10 +1,8 @@
 import { useQuery, gql } from "@apollo/client";
 import { useRouter } from "next/router";
 
-import { Wrapper, WriterWrapper, WriterWrapperLeft, WriterWrapperRight, ProfilImage, 
-          WriterDate, Location, Address1, Address2, Icon, FileIcon, MapIcon,
-        Writer, Date, InputWrapper, Title, Contents, Like, 
-        LikeImage, LikeCount, Dislike, DislikeImage, DislikeCount,
+import { Wrapper, WriterWrapper, WriterWrapperLeft, WriterWrapperRight,   
+        ProfilImage, WriterDate, Location, Triangle, Address1, Address2, Icon, FileIcon, MapIcon, Writer, Date, InputWrapper, Title, ContentsImage, Contents,Youtube,ButtonWrapper, Like, LikeImage, LikeCount, Dislike, DislikeImage, DislikeCount,
         } from '../../../../styles/router_detail_read'
 
 import '@fortawesome/fontawesome-free/js/all.js';
@@ -31,7 +29,7 @@ export default function BoardRouter_DetailRead() {
             <WriterWrapper>
                 <WriterWrapperLeft>
                     <ProfilImage>
-                    <i class="fas fa-user-circle fa-2x"></i>
+                    <i width="46.67" height="46.67" class="fas fa-user-circle fa-2x"></i>
                     </ProfilImage>
                     <WriterDate>
                         <Writer>{data?.fetchBoard.writer}</Writer>
@@ -42,31 +40,32 @@ export default function BoardRouter_DetailRead() {
                     <Location>
                         <Address1>서울특별 영등포구 양산로 200</Address1>
                         <Address2>(영등포동5가,영등포시장역)영등포 타임스퀘어 2층</Address2>
-                        <Icon>
-                            <FileIcon><img src="  "/></FileIcon>
-                            <MapIcon><img src="  "/></MapIcon>
-                        </Icon>
                     </Location>
+                    <Triangle></Triangle>   
+                    <Icon>
+                        <FileIcon><i class="fas fa-infinity"></i></FileIcon>
+                        <MapIcon><i class="fas fa-map-marker-alt"></i></MapIcon>
+                    </Icon>
                 </WriterWrapperRight>  
             </WriterWrapper>
             <InputWrapper>
                 <Title>{data?.fetchBoard.title}</Title>
-                <img src="/posty.jpeg" />
+                <ContentsImage><img width="996" height="480" src="/posty.jpeg"/></ContentsImage>
                 <Contents>{data?.fetchBoard.contents}</Contents>
             </InputWrapper>
-            <InputWrapper>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/ScWXi59imGI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </InputWrapper>
-            <InputWrapper>
+            <Youtube>
+            <iframe width="486" height="240" src="https://www.youtube.com/embed/ScWXi59imGI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </Youtube>
+            <ButtonWrapper>
                 <Like>
-                    <LikeImage><img src="  "/></LikeImage>
+                    <LikeImage><i width="22" height="20" class="far fa-thumbs-up"></i></LikeImage>
                     <LikeCount>1920</LikeCount>
                 </Like>
                 <Dislike>
-                    <DislikeImage><img src="  "/></DislikeImage>
+                    <DislikeImage><i width="22" height="20" class="far fa-thumbs-down"></i></DislikeImage>
                     <DislikeCount>1920</DislikeCount>
                 </Dislike>
-            </InputWrapper>
+            </ButtonWrapper>
         </Wrapper>
 
         
--- freeboard_frontend/styles/router_detail_read.js
+++ freeboard_frontend/styles/router_detail_read.js
@@ -32,7 +32,9 @@ export const WriterWrapperLeft = styled.div`
 `
 
 export const WriterWrapperRight = styled.div`
-    width: 376px;
+    width: 392px;
+    /* border: 1px solid red; */
+    padding-left: 15px;
 
 `
 
@@ -49,7 +51,33 @@ export const WriterDate = styled.div`
 `
 
 export const Location = styled.div`
+    width: 368px;
+    height: 64px;
+    border: 1px solid #898989;
+    background-color: #898989;
+    color: white;
+    padding-left: 16px;
+    padding-top: 8px;
+    padding-right: 16px;
+    padding-bottom: 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    font-size: 16px;
+    font-weight: 400;
+    font-style: normal;
+    line-height: 23.68px;
+    
+`
 
+export const Triangle = styled.div`
+    width: 0px;
+    height: 0px;
+    border-top: 6px solid #898989;
+    border-right: 8px solid #898989;
+    border-bottom: 6px solid transparent;
+    border-left: 8px solid transparent;
+    margin-left: 352px;
 `
 
 export const Address1 = styled.div`
@@ -61,23 +89,37 @@ export const Address2 = styled.div`
 `
 
 export const Icon = styled.div`
-
+    /* padding-top: 19.33px; */
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    /* align-items: center; */
 `
 
 export const FileIcon = styled.div`
-
+    color: #FFD600;
+    padding-right: 29.33px;
+    /* padding-top: 19.33px; */
 `
 
 export const MapIcon = styled.div`
-
+    color: #FFD600;
+    /* padding-top: 4.67px; */
 `
 
 export const Writer = styled.div`
+    font-size: 24px;
+    font-weight: 500;
+    line-height: 35.52px;
+    font-style: normal;
 
 `
 
 export const Date = styled.div`
-
+    font-size: 16px;
+    font-weight: 400;
+    font-style: normal;
+    line-height: 23.68px;
 `
 
 export const InputWrapper = styled.div`
@@ -85,36 +127,72 @@ export const InputWrapper = styled.div`
 `
 
 export const Title = styled.div`
-    border: 3px solid red;
+    /* border: 3px solid red; */
     font-size: 36px;
+    font-weight: 700;
     padding-top: 80px;
+    padding-bottom: 40px;
 
+`
+
+export const ContentsImage = styled.div`
+    width: 996px;
+    height: 480px;
 `
 
 export const Contents = styled.div`
+    padding-top: 40px;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 23.68px;
+`
+export const Youtube = styled.div`
+    display: flex;
+    justify-content: center;
+    padding-top: 120px;
+`
 
+export const ButtonWrapper = styled.div`
+    padding-top: 162px;
+    display: flex;
+    flex-direction: row;
 `
 
 export const Like = styled.div`
-
-`
+    padding-right: 59px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`   
 
 export const LikeImage = styled.div`
-
+    color: #FFD600;
 `
 
 export const LikeCount = styled.div`
-
+    color: #FFD600;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 26.64px;
+    font-style: normal;
 `
 
 export const Dislike = styled.div`
-
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `
 
 export const DislikeImage = styled.div`
-
+    color: #828282;
 `
 
 export const DislikeCount = styled.div`
-
+    color: #828282;
+    font-size: 18px;
+    font-weight: 400;
+    font-style: normal;
+    line-height: 26.64px;
 `
