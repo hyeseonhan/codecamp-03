c8412b64817a199861b750c64ba226b7e18b7def
hyeseon <h1225hs@gmail.com>
2021-09-29 10:44:31 +0900

    19일차 프로그래머스

--- programmers/5주차/index.js
+++ programmers/5주차/index.js
@@ -127,3 +127,64 @@ function solution(a, b) {
   const days = new Date(2016, a - 1, b).getDay();
   return week[days];
 }
+
+// 최대공약수와 최소공배수
+
+function solution(n, m) {
+  //최대공약수: 두 수의 약수 중에서 제일 큰수
+  //최소공배수: 두 수의 배수 중에서 제일 작은 수
+  let answer = [];
+
+  //공통되는 약수를 담아주는 배열
+  const gcdArr = [];
+
+  //최대공약수 구하기
+  for (let i = 1; i <= m; i++) {
+    if (n % i === 0 && m % i === 0) {
+      gcdArr.push(i);
+    }
+  }
+  answer[0] = Math.max(...gcdArr);
+
+  //최소공배수 구하기
+  for (let l = m; l <= n * m; l += m) {
+    if (l % n === 0) {
+      answer[1] = l;
+      break; //반복문 끝
+    }
+  }
+  return answer;
+}
+
+function greatestCommonDivisor(a, b) {
+  return b ? greatestCommonDivisor(b, a % b) : Math.abs(a);
+}
+function leastCommonMultipleOfTwo(a, b) {
+  return (a * b) / greatestCommonDivisor(a, b);
+}
+function gcdlcm(a, b) {
+  return [greatestCommonDivisor(a, b), leastCommonMultipleOfTwo(a, b)];
+}
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log(gcdlcm(3, 12));
+
+// 유클리드 호재범
+// a를 b로 나눳을 때 (a,가 b 보다가 클 경우 ) === 큰 수에서 작은 수를 나웠을 때
+// 나머지 값이 0 이 되면, 작은 수가 최소공배수가 된다.
+// 0이 되지 않으면 작은 수가 큰 수가 되고, 나머지 값이 작은 수가 된다.
+// 반복했을 때에 0이 나오면, 작은 수가 최소공배수가 된다.
+function solution(n, m) {
+  let a = m; //큰수
+  let b = n; //큰수
+  let r = 0; //a를 b  로 나눴을 때에 나머지 값을 저장
+
+  while (a % b > 0) {
+    r = a % b;
+    a = b; //큰 수에 작은 수를 할당
+    b = r; //작은 수에 나머지 값 할당
+  }
+
+  //최소공배수는 두 수를 곱한 값에 최대공약수를 나눈 값
+  return [b, (n * m) / b];
+}
