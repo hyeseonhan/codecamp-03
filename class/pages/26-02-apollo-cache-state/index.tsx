2244d1888fb441ce0a5ea93fe863999fe90e3741
hyeseon <h1225hs@gmail.com>
2021-10-13 16:49:13 +0900

    퀴즈 26일차 apollo-cache-state

--- class/pages/26-02-apollo-cache-state/index.tsx
+++ class/pages/26-02-apollo-cache-state/index.tsx
@@ -1,12 +1,11 @@
 import { gql, useMutation, useQuery } from "@apollo/client";
-
 const FETCH_BOARDS = gql`
   query fetchBoards {
     fetchBoards {
-      _id
       writer
       title
       contents
+      _id
     }
   }
 `;
@@ -29,28 +28,29 @@ const CREATE_BOARD = gql`
 `;
 
 export default function ApolloCacheStatePage() {
-  const { data } = useQuery(FETCH_BOARDS); // return에 뿌려주기 위해서
+  const { data } = useQuery(FETCH_BOARDS);
   const [deleteBoard] = useMutation(DELETE_BOARD);
   const [createBoard] = useMutation(CREATE_BOARD);
 
   const onClickDelete = (boardId) => async () => {
+    // 가장 가까운 함수에 async 붙여줘야함
+    // boardId
     await deleteBoard({
       variables: {
         boardId: boardId,
       },
       update(cache, { data }) {
-        const deletedId = data.dleteBoard;
-
+        // update는 위에 요청이 끝나면 실행된다.
+        const deletedId = data.deleteBoard;
         cache.modify({
           fields: {
             fetchBoards: (prev, { readField }) => {
               // 1. 기존의 fetchBoards 10개에서, 지금 삭제된 ID를 제외한 9개를 만들고
-              // 2. 그렇게 만들어진 9개의 새로운 fetchBoards를 return하여, 덮어씌우기
+              // 2. 그렇게 만들어진 9개의 새로운 fetchBoards 를 return 하여, 덮어씌우기
 
               const newFetchBoards = prev.filter(
                 (el) => readField("_id", el) !== deletedId
               );
-
               return [...newFetchBoards];
             },
           },
@@ -63,10 +63,10 @@ export default function ApolloCacheStatePage() {
     createBoard({
       variables: {
         createBoardInput: {
-          writer: "곽두팔",
-          password: "11",
-          title: "두팔이올시다",
-          contents: "두팔두팔",
+          writer: "테스트",
+          password: "123",
+          title: "테스트제목",
+          contents: "테스트내용",
         },
       },
       update(cache, { data }) {
@@ -74,7 +74,7 @@ export default function ApolloCacheStatePage() {
           fields: {
             fetchBoards: (prev) => {
               // 추가된 createBoard 결과물과 이전의 10개를 합쳐서 11개를 돌려주기
-              return [...prev, data.createBoard];
+              return [data.createBoard, ...prev]; // data.createBoard 를 뒤에 넣으다면 뒤에 새로 생기게된다.
             },
           },
         });
@@ -82,11 +82,10 @@ export default function ApolloCacheStatePage() {
     });
   };
 
-  // gql로 만든 FETCH_BOARDS 를 뿌려준다.
   return (
     <>
-      {data?.fetchBoards.map((el, index) => (
-        <div key={index}>
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
           <span>{el.writer}</span>
           <span>{el.title}</span>
           <span>{el.contents}</span>
@@ -97,3 +96,103 @@ export default function ApolloCacheStatePage() {
     </>
   );
 }
+
+// import { gql, useMutation, useQuery } from "@apollo/client";
+
+// const FETCH_BOARDS = gql`
+//   query fetchBoards {
+//     fetchBoards {
+//       _id
+//       writer
+//       title
+//       contents
+//     }
+//   }
+// `;
+
+// const DELETE_BOARD = gql`
+//   mutation deleteBoard($boardId: ID!) {
+//     deleteBoard(boardId: $boardId)
+//   }
+// `;
+
+// const CREATE_BOARD = gql`
+//   mutation createBoard($createBoardInput: CreateBoardInput!) {
+//     createBoard(createBoardInput: $createBoardInput) {
+//       _id
+//       writer
+//       title
+//       contents
+//     }
+//   }
+// `;
+
+// export default function ApolloCacheStatePage() {
+//   const { data } = useQuery(FETCH_BOARDS); // return에 뿌려주기 위해서
+//   const [deleteBoard] = useMutation(DELETE_BOARD);
+//   const [createBoard] = useMutation(CREATE_BOARD);
+
+//   const onClickDelete = (boardId) => async () => {
+//     await deleteBoard({
+//       variables: {
+//         boardId: boardId,
+//       },
+//       update(cache, { data }) {
+//         const deletedId = data.dleteBoard;
+
+//         cache.modify({
+//           fields: {
+//             fetchBoards: (prev, { readField }) => {
+//               // 1. 기존의 fetchBoards 10개에서, 지금 삭제된 ID를 제외한 9개를 만들고
+//               // 2. 그렇게 만들어진 9개의 새로운 fetchBoards를 return하여, 덮어씌우기
+
+//               const newFetchBoards = prev.filter(
+//                 (el) => readField("_id", el) !== deletedId
+//               );
+
+//               return [...newFetchBoards];
+//             },
+//           },
+//         });
+//       },
+//     });
+//   };
+
+//   const onClickCreate = () => {
+//     createBoard({
+//       variables: {
+//         createBoardInput: {
+//           writer: "곽두팔",
+//           password: "11",
+//           title: "두팔이올시다",
+//           contents: "두팔두팔",
+//         },
+//       },
+//       update(cache, { data }) {
+//         cache.modify({
+//           fields: {
+//             fetchBoards: (prev) => {
+//               // 추가된 createBoard 결과물과 이전의 10개를 합쳐서 11개를 돌려주기
+//               return [...prev, data.createBoard];
+//             },
+//           },
+//         });
+//       },
+//     });
+//   };
+
+//   // gql로 만든 FETCH_BOARDS 를 뿌려준다.
+//   return (
+//     <>
+//       {data?.fetchBoards.map((el, index) => (
+//         <div key={index}>
+//           <span>{el.writer}</span>
+//           <span>{el.title}</span>
+//           <span>{el.contents}</span>
+//           <button onClick={onClickDelete(el._id)}>삭제하기</button>
+//         </div>
+//       ))}
+//       <button onClick={onClickCreate}>등록하기</button>
+//     </>
+//   );
+// }
--- class/pages/quiz/26-apollo-cache-state/index.tsx
+++ class/pages/quiz/26-apollo-cache-state/index.tsx
@@ -5,7 +5,7 @@ const FETCH_BOARDS = gql`
   query fetchBoards {
     fetchBoards {
       _id
-      wirter
+      writer
       title
       contents
     }
@@ -23,37 +23,83 @@ const CREATE_BOARD = gql`
   }
 `;
 
+const DELETE_BOARD = gql`
+  mutation deleteBoard($boardId: ID!) {
+    deleteBoard(boardId: $boardId)
+  }
+`;
+
 export default function QuizApolloCacheState() {
   const { handleSubmit, register } = useForm();
 
-  //   function onClickCreate(data) {
-  //     console.log(data);
-  //   }
-
   const { data } = useQuery(FETCH_BOARDS);
   const [createBoard] = useMutation(CREATE_BOARD);
+  const [deleteBoard] = useMutation(DELETE_BOARD);
 
-  const onClickCreate = () => {
-    createBoard({
+  const onClickDelete = (boardId) => async () => {
+    await deleteBoard({
       variables: {
-        writer,
-        title,
-        contents,
+        boardId: boardId,
+      },
+      update(cache, { data }) {
+        const deletedId = data.deldteBoard;
+        cache.modify({
+          fields: {
+            fetchBoards: (prev, { readField }) => {
+              const newFetchBoards = prev.filter(
+                (el) => readField("_id", el) !== deletedId
+              );
+              return [...newFetchBoards];
+            },
+          },
+        });
       },
     });
   };
 
+  async function onClickCreate(data) {
+    // try {
+    await createBoard({
+      variables: {
+        createBoardInput: {
+          writer: data.writer,
+          password: data.password,
+          title: data.title,
+          contents: data.contents,
+        },
+      },
+      update(cache, { data }) {
+        cache.modify({
+          fields: {
+            fetchBoards: (prev) => {
+              return [...prev, data.createBoard];
+            },
+          },
+        });
+      },
+    });
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  }
+
   return (
     <form onSubmit={handleSubmit(onClickCreate)}>
       작성자: <input type="text" {...register("writer")} />
+      <br />
+      비밀번호: <input type="password" {...register("password")} />
+      <br />
       제목: <input type="text" {...register("title")} />
+      <br />
       내용: <input type="text" {...register("contents")} />
+      <br />
       <button type="submit">등록하기</button>
-      {data?.fetchBoards.map((el, index) => (
-        <div key={index}>
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
           작성자: <span>{el.writer}</span>
           제목: <span>{el.title}</span>
           내용: <span>{el.contents}</span>
+          <button onClick={onClickDelete(el._id)}>X</button>
         </div>
       ))}
     </form>
