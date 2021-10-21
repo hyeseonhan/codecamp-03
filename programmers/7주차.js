0d42ce2478f6d1b11bb15eefb8ca393ebb293e4e
hyeseon <h1225hs@gmail.com>
2021-10-21 11:19:06 +0900

    34일차 프로그래머스

--- programmers/7주차.js
+++ programmers/7주차.js
@@ -164,3 +164,197 @@ function solution(dartResult) {
 
   return darts.reduce((a, b) => a + b);
 }
+
+// 신규 아이디 추천
+
+const filter = "qwertyuiopasdfghjklzxcvbnm1234567890-_.";
+
+function solution(new_id) {
+  // 1단계: 대문자를 -> 소문자로 치환
+  new_id = new_id.toLowerCase();
+  //  console.log(new_id)
+
+  let result = "";
+  // 2단계 : 소문자, 숫자, 빼기, 밑줄 마침표를 제외한 모든 문자 제거
+  for (let i = 0; i < new_id.longth; i++) {
+    // console.log(new_id[i], filter.includes( new_id[i]))
+    if (filter.includes(new_id[i])) {
+      result += new_id[i];
+    }
+  }
+  // console.log(result)
+
+  // 3단계: .가 연속으로 들어오면 .으로 치환 (.. => .)
+  while (result.includes("..")) {
+    // console.log(result)
+    result = result.replace("..", ".");
+  }
+  // console.log(result)
+
+  // 4단계 : .가 처음이나 끝에 위치한다면 제거
+  if (result[0] === ".") {
+    result = result.substr(1); // substr(1) 문자열의 첫번째를 지울수 있다
+  }
+
+  function removeLastDot() {
+    if (result[result.length - 1] === ".") {
+      result = result.slice(0, result.length - 1);
+    }
+  }
+  removeLastDot();
+  // console.log(result)
+
+  // 5단계 : 빈 문자열이라면 "a" 대입
+  if (result === "") {
+    result = "a";
+  }
+  // console.log(result)
+
+  // 6단계 : 문자열 길이가 16 이상이면 15의 길이값을 가지는 문자열로 치환( = 15 길이까지 자른다)
+  //        제거 후에, 무침표가 끝에 위치하면 제거
+  if (result.length >= 16) {
+    result = result.slice(0, 15);
+    console.log(result);
+    removeLastDot();
+  }
+  // console.log(result)
+
+  // 7단계 : 문자열 길이가 2자 이하라면, 마지막 글자를 3이 될때까지 반복해서 추가
+  if (result.length <= 2) {
+    result = result.padEnd(3, result[result.length - 1]);
+  }
+  // console.log(result)
+
+  return result;
+}
+
+// 배열 메서드
+
+const filter = "qwertyuiopasdfghjklzxcvbnm1234567890-_.";
+
+function solution(new_id) {
+  // 1단계: 대문자를 -> 소문자로 치환
+  new_id = new_id.toLowerCase();
+
+  // 2단계 : 소문자, 숫자, 빼기, 밑줄 마침표를 제외한 모든 문자 제거
+  let result = new_id.split("").filter((str) => filter.includes(str));
+
+  // 3단계: .가 연속으로 들어오면 .으로 치환 (.. => .)
+  result = result.filter(
+    (str, i) => (str === "." && result[i + 1] !== ".") || str !== "."
+  );
+
+  // 4단계 : .가 처음이나 끝에 위치한다면 제거
+  if (result[0] === ".") {
+    result.splice(0, 1);
+  }
+  function removeLastDot() {
+    if (result[result.length - 2] === ".") {
+      result.splice(result.length - 1, 1);
+    }
+  }
+  removeLastDot();
+
+  // 5단계 : 빈 문자열이라면 "a"
+  if (result.length === 0) {
+    result = ["a"];
+  }
+  // 6단계 : 문자열 길이가 16 이상이면 15의 길이값을 가지는 문자열로 치환( = 15 길이까지 자른다)
+  //        제거 후에, 무침표가 끝에 위치하면 제거\
+  if (result.length >= 16) {
+    result = result.slice(0, 15);
+    removeLastDot();
+  }
+  // console.log(result);
+  // 7단계 : 문자열 길이가 2자 이하라면, 마지막 글자를 3이 될때까지 반복해서 추가
+  if (result.length <= 2) {
+    const add = new Array(3 - result.length).fill(result[result.length - 1]);
+    // console.log(add);
+    result = result.concat(add);
+  }
+  // console.log(result);
+  return result.join("");
+}
+
+function solution(new_id) {
+  let engTest = /^[a-zA-Z]*$/;
+  let num = /^[0-9]*$/;
+
+  let newID = new_id
+    .split("")
+    .map((el, idx) => {
+      if (engTest.test(el)) {
+        return el.toLowerCase();
+      } else {
+        return el;
+      }
+    })
+    .filter((el) => {
+      return (
+        el === "-" ||
+        el === "_" ||
+        el === "." ||
+        engTest.test(el) ||
+        num.test(el)
+      );
+    });
+  newID = newID.join("");
+  while (newID.includes("..")) {
+    newID = newID.replace("..", ".");
+  }
+  if (newID[0] === ".") {
+    newID = newID.substr(1);
+  }
+  if (newID[newID.length - 1] === ".") {
+    newID = newID.slice(0, newID.length);
+  }
+  if (newID.length === 0) {
+    newID += "a";
+  }
+  if (newID.length >= 16) {
+    newID = newID.slice(0, 15);
+  }
+  if (newID[newID.length - 1] === ".") {
+    newID = newID.slice(0, newID.length - 1);
+  }
+  while (newID.length < 3) {
+    newID += newID[newID.length - 1];
+  }
+  return newID;
+}
+
+function solution(new_id) {
+  const answer = new_id
+    .toLowerCase() // 1
+    .replace(/[^\w-_.]/g, "") // 2
+    .replace(/\.+/g, ".") // 3
+    .replace(/^\.|\.$/g, "") // 4
+    .replace(/^$/, "a") // 5
+    .slice(0, 15)
+    .replace(/\.$/, ""); // 6
+  const len = answer.length;
+  return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
+}
+
+const solution = (new_id) => {
+  const id = new_id
+    .toLowerCase()
+    .replace(/[^\w\d-_.]/g, "")
+    .replace(/\.{2,}/g, ".")
+    .replace(/^\.|\.$/g, "")
+    .padEnd(1, "a")
+    .slice(0, 15)
+    .replace(/^\.|\.$/g, "");
+  return id.padEnd(3, id[id.length - 1]);
+};
+
+const solution = (new_id) =>
+  new_id
+    .toLowerCase()
+    .replace(/[^\w-_.]/g, "")
+    .replace(/\.+/g, ".")
+    .replace(/^\.|\.$/g, "")
+    .replace(/^$/, "a")
+    .match(/^.{0,14}[^.]/)[0]
+    .replace(/^(.)$/, "$1$1$1")
+    .replace(/^(.)(.)$/, "$1$2$2");
