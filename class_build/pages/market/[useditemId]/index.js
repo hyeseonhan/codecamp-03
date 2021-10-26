68d6ceaba483fc617f698426e0b084a55d0ae7cb
hyeseon <h1225hs@gmail.com>
2021-10-26 13:57:33 +0900

    ddd

--- class_build/pages/market/[useditemId]/index.js
+++ class_build/pages/market/[useditemId]/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { gql, request } from "graphql-request";
 
-export default function MarketPage() {
+export default function MarketPage(props) {
   return (
     <>
       <Head>
