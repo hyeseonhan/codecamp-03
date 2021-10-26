0b4879fdd7bfebad5b0d02ef243cf2a60e819f1d
hyeseon <h1225hs@gmail.com>
2021-10-26 14:02:34 +0900

    modify-2

--- class_build/pages/boards/index.js
+++ class_build/pages/boards/index.js
@@ -8,7 +8,7 @@ export default function BoardsPage() {
         <meta property="og:description" content="환영합니다~" />
         <meta
           property="og:image"
-          content="https://w.namu.la/s/bc75175b063a43a123523fba625ed9d185f6a180a3c1e9ae9409db0c11026[…]a5c1fde8b3327cb27431716d9f89b84b1d8945e6fc9eb60ceb418432323cd459"
+          content="https://w.namu.la/s/bc75175b063a43a123523fba625ed9d185f6a180a3c1e9ae9409db0c110266e4f69e7aa434253687819b7dd5b36cccf4bd508977f6f76e3b9353db545d75168ba5c1fde8b3327cb27431716d9f89b84b1d8945e6fc9eb60ceb418432323cd459"
         />
       </Head>
       <div>게시판입니다.</div>
