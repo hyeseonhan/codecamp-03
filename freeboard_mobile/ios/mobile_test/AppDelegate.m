9a5e37df553e1d12df3124ba823bc31d9a96f115
hyeseon <h1225hs@gmail.com>
2021-11-02 11:07:17 +0900

    React-Native 연습/git commit연습

--- freeboard_mobile/ios/mobile_test/AppDelegate.m
+++ freeboard_mobile/ios/mobile_test/AppDelegate.m
@@ -33,7 +33,7 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"freeboard_mobile"
+                                                   moduleName:@"mobile_test"
                                             initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
--- freeboard_mobile/ios/mobile_testTests/mobile_testTests.m
+++ freeboard_mobile/ios/mobile_testTests/mobile_testTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface freeboard_mobileTests : XCTestCase
+@interface mobile_testTests : XCTestCase
 
 @end
 
-@implementation freeboard_mobileTests
+@implementation mobile_testTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
