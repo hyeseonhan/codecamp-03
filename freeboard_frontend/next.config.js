c5cdb43a498d011d8f226362e89057a3af65778f
hyeseonhan <h1225hs@gmail.com>
2021-12-14 23:16:54 +0900

    Edit ssr

--- freeboard_frontend/next.config.js
+++ freeboard_frontend/next.config.js
@@ -11,5 +11,7 @@ module.exports = {
     "/images": { page: "/images" },
     "/main": { page: "/main" },
     "/mypage": { page: "/mypage" },
+    "/market": { page: "/market" },
+    "/boards": { page: "/boards" },
   }),
 };
