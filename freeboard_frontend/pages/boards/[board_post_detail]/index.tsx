b807fa0a613452579ffbaec2703bf75ffc584502
hyeseonhan <h1225hs@gmail.com>
2021-12-14 16:26:59 +0900

    build ssr

--- freeboard_frontend/pages/boards/[board_post_detail]/index.tsx
+++ freeboard_frontend/pages/boards/[board_post_detail]/index.tsx
@@ -1,13 +1,51 @@
 import BoardDetail from "../../../src/components/units/board/read/BoardDetail.container";
 import BoardCommentWrite from "../../../src/components/units/board/comment/write/BoardCommentWrite.container";
 import BoardCommentList from "../../../src/components/units/board/comment/list/BoardCommentList.container";
+import Head from "next/head";
+import { gql, request } from "graphql-request";
 
-export default function BoardDetailPage() {
+export default function BoardDetailPage(props: any) {
   return (
     <>
+      <Head>
+        <meta property="og:title" content={props.fetchBoard.name} />
+        <meta property="og:description" content={props.fetchBoard.writer} />
+        <meta
+          property="og:image"
+          content={`https://storage.googleapis.com/${props.fetchBoard.images[0]}`}
+        />
+      </Head>
       <BoardDetail />
       <BoardCommentWrite />
       <BoardCommentList />
     </>
   );
 }
+
+export const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      _id
+      writer
+      title
+      images
+      contents
+    }
+  }
+`;
+
+export const getServerSideProps = async (context: any) => {
+  // 1. graphql 데이터를 요청
+  const result = await request(
+    "https://backend03.codebootcamp.co.kr/graphql08",
+    FETCH_BOARD,
+    { boardId: context.query.board_post_detail }
+  );
+
+  // 2. 요청받은 데이터를 페이지로 넘긴다.
+  return {
+    props: {
+      fetchBoard: result.fetchBoard,
+    },
+  };
+};
--- freeboard_frontend/pages/market/product-detail/[useditemId]/index.tsx
+++ freeboard_frontend/pages/market/product-detail/[useditemId]/index.tsx
@@ -2,8 +2,10 @@ import ProductDetail from "../../../../src/components/units/market/detail/Produc
 import ProductCommentWrite from "../../../../src/components/units/market/comment/write/ProductCommentWrite.container";
 import ProductCommentList from "../../../../src/components/units/market/comment/list/ProductCommentlist.container";
 import { useState } from "react";
+import Head from "next/head";
+import { gql, request } from "graphql-request";
 
-export default function ProductDetailPage() {
+export default function ProductDetailPage(props: any) {
   // 대댓글 아이콘 email로 비교해서 조건부렌더링하기 위해 state 끌어올릴 때 사용
   const [isReplySeller, setIsReplySeller] = useState("");
 
@@ -12,6 +14,14 @@ export default function ProductDetailPage() {
 
   return (
     <>
+      <Head>
+        <meta property="og:title" content={props.fetchUseditem.name} />
+        <meta property="og:description" content={props.fetchUseditem.remarks} />
+        <meta
+          property="og:image"
+          content={`https://storage.googleapis.com/${props.fetchUseditem.images[0]}`}
+        />
+      </Head>
       <ProductDetail />
       <ProductCommentWrite />
       <ProductCommentList
@@ -23,3 +33,29 @@ export default function ProductDetailPage() {
     </>
   );
 }
+
+const FETCH_USEDITEM = gql`
+  query fetchUseditem($useditemId: ID!) {
+    fetchUseditem(useditemId: $useditemId) {
+      name
+      remarks
+      images
+    }
+  }
+`;
+
+export const getServerSideProps = async (context: any) => {
+  const result = await request(
+    "https://backend03.codebootcamp.co.kr/graphql08",
+    FETCH_USEDITEM,
+    {
+      useditemId: context.query.useditemId,
+    }
+  );
+
+  return {
+    props: {
+      fetchUseditem: result.fetchUseditem,
+    },
+  };
+};
