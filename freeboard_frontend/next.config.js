d2dc88cb91947d75010c1bf34942396473708cfa
hyeseonhan <h1225hs@gmail.com>
2021-12-15 03:00:52 +0900

    Edit ssr

--- freeboard_frontend/next.config.js
+++ freeboard_frontend/next.config.js
@@ -4,12 +4,11 @@ module.exports = {
   generateBuildId: () => "freeboard_frontend",
   exportPathMap: () => ({
     "/": { page: "/" },
-    "/_next": { page: "/_next" },
+
     "/404": { page: "/404" },
 
     "/home": { page: "/home" },
 
     "/main": { page: "/main" },
-    "/mypage": { page: "/mypage" },
   }),
 };
