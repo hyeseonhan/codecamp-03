37f7e7bafe658dbc8d6b88a7d94b5a760f326073
hyeseonhan <h1225hs@gmail.com>
2021-12-15 17:10:49 +0900

    Edit ssr

--- freeboard_frontend/next.config.js
+++ freeboard_frontend/next.config.js
@@ -6,6 +6,9 @@ module.exports = {
     "/": { page: "/" },
     "/404": { page: "/404" },
 
+    "/boards/[board_post_detail]/edit": {
+      page: "/boards/[board_post_detail]/edit",
+    },
     "/boards/board-post": { page: "/boards/board-post" },
     "/boards/list": { page: "/boards/list" },
     "/boards/openapi": { page: "/boards/openapi" },
@@ -13,6 +16,9 @@ module.exports = {
     "/home": { page: "/home" },
     "/main": { page: "/main" },
 
+    "/market/product-detail/[useditemId]/edit": {
+      page: "/market/product-detail/[useditemId]/edit",
+    },
     "/market/flickity": { page: "/market/flickity" },
     "/market/product-list": { page: "/market/product-list" },
     "/market/product-post": { page: "/market/product-post" },
