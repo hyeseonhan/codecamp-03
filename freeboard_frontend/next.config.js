47c8bcfb589513c60fbe63e097d42e0cee9975c6
hyeseonhan <h1225hs@gmail.com>
2021-12-16 02:51:39 +0900

    Edit ssr

--- freeboard_frontend/next.config.js
+++ freeboard_frontend/next.config.js
@@ -2,33 +2,33 @@ module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
   assetPrefix: "/ssr",
-  // generateBuildId: () => "freeboard_frontend",
-  // exportPathMap: () => ({
-  //   "/": { page: "/" },
-  //   "/404": { page: "/404" },
+  generateBuildId: () => "freeboard_frontend",
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/404": { page: "/404" },
 
-  //   "/boards/[board_post_detail]/edit": {
-  //     page: "/boards/[board_post_detail]/edit",
-  //   },
-  //   "/boards/board-post": { page: "/boards/board-post" },
-  //   "/boards/list": { page: "/boards/list" },
-  //   "/boards/openapi": { page: "/boards/openapi" },
+    "/boards/[board_post_detail]/edit": {
+      page: "/boards/[board_post_detail]/edit",
+    },
+    "/boards/board-post": { page: "/boards/board-post" },
+    "/boards/list": { page: "/boards/list" },
+    "/boards/openapi": { page: "/boards/openapi" },
 
-  //   "/home": { page: "/home" },
-  //   "/main": { page: "/main" },
+    "/home": { page: "/home" },
+    "/main": { page: "/main" },
 
-  //   "/market/product-detail/[useditemId]/edit": {
-  //     page: "/market/product-detail/[useditemId]/edit",
-  //   },
-  //   "/market/flickity": { page: "/market/flickity" },
-  //   "/market/product-list": { page: "/market/product-list" },
-  //   "/market/product-post": { page: "/market/product-post" },
+    "/market/product-detail/[useditemId]/edit": {
+      page: "/market/product-detail/[useditemId]/edit",
+    },
+    "/market/flickity": { page: "/market/flickity" },
+    "/market/product-list": { page: "/market/product-list" },
+    "/market/product-post": { page: "/market/product-post" },
 
-  //   // "/mypage/createacc": { page: "/mypage" },
-  //   // "/mypage/login": { page: "/mypage" },
-  //   // "/mypage/loginfo": { page: "/mypage" },
-  //   // "/mypage/mymarket": { page: "/mypage" },
-  //   // "/mypage/mypoint": { page: "/mypage" },
-  //   // "/mypage/myprofile": { page: "/mypage" },
-  // }),
+    // "/mypage/createacc": { page: "/mypage" },
+    // "/mypage/login": { page: "/mypage" },
+    // "/mypage/loginfo": { page: "/mypage" },
+    // "/mypage/mymarket": { page: "/mypage" },
+    // "/mypage/mypoint": { page: "/mypage" },
+    // "/mypage/myprofile": { page: "/mypage" },
+  }),
 };
