9f7ab9da2a828ce2ed7b4aea5c48cda09283bc35
hyeseonhan <h1225hs@gmail.com>
2021-12-15 16:19:12 +0900

    Edit ssr

--- freeboard_frontend/next.config.js
+++ freeboard_frontend/next.config.js
@@ -4,11 +4,23 @@ module.exports = {
   generateBuildId: () => "freeboard_frontend",
   exportPathMap: () => ({
     "/": { page: "/" },
-
     "/404": { page: "/404" },
+    "/boards/board-post": { page: "/boards/board-post" },
+    "/boards/list": { page: "/boards/list" },
+    "/boards/openapi": { page: "/boards/openapi" },
 
     "/home": { page: "/home" },
-
     "/main": { page: "/main" },
+
+    "/market/flickity": { page: "/market/flickity" },
+    "/market/product-list": { page: "/market/product-list" },
+    "/market/product-post": { page: "/market/product-post" },
+
+    "/mypage/createacc": { page: "/mypage" },
+    "/mypage/login": { page: "/mypage" },
+    "/mypage/loginfo": { page: "/mypage" },
+    "/mypage/mymarket": { page: "/mypage" },
+    "/mypage/mypoint": { page: "/mypage" },
+    "/mypage/myprofile": { page: "/mypage" },
   }),
 };
