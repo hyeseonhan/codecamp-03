f0e97ce3708d2bcb11165bbc79aa1457ab319249
hyeseonhan <h1225hs@gmail.com>
2021-12-18 15:23:58 +0900

    Edit withAuth

--- freeboard_frontend/pages/boards/board-post/index.tsx
+++ freeboard_frontend/pages/boards/board-post/index.tsx
@@ -1,9 +1,5 @@
-// import { withAuth } from "../../../src/components/commons/hocs/withAuth";
 import BoardWrite from "../../../src/components/units/board/write/BoardWrite.container";
 
-// const BoardWritePage = () => {
 export default function BoardWritePage() {
   return <BoardWrite isEdit={false} />;
 }
-
-// export default withAuth(BoardWritePage);
